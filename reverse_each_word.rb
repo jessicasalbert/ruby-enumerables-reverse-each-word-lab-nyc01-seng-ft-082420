cc609852a160a997ef0bc5cf891574bf87a892b3
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-07 14:51:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,8 +7,8 @@ def reverse_each_word(sentence)
   reversed.join(" ")
 end
 
-# def reverse_each_word(sentence)
-#   words = sentence.split(" ")
-#   reversed = words.collect { |word| word.reverse }
-#   reversed.join(" ")
-# end
+def reverse_each_word(sentence)
+  words = sentence.split(" ")
+  reversed = words.collect { |word| word.reverse }
+  reversed.join(" ")
+end
