14063d7a66754ce1e13f8340f55cd8feaa56ca60
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-07 14:45:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentences = sentence.split(" ")
   sentences.each do |word|
     letters = word.split("")
-    reverse = letters.reverse
+    reverse = letters.reverse()
     reverse = reverse.join("")
   end
   return reverse.join(" ")
