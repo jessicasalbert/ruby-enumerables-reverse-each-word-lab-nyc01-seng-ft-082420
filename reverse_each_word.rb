4b67f8baeb3e9e79a4b27ed99bc6996056b930ab
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-07 14:51:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,11 +4,11 @@ def reverse_each_word(sentence)
   sentences.each do |word|
     reversed.push(word.reverse)
   end
-  return reversed.join(" ")
-end
-
-def reverse_each_word(sentence)
-  words = sentence.split(" ")
-  reversed = words.collect { |word| word.reverse }
   reversed.join(" ")
 end
+
+# def reverse_each_word(sentence)
+#   words = sentence.split(" ")
+#   reversed = words.collect { |word| word.reverse }
+#   reversed.join(" ")
+# end
